6ef2530d8cb5553dadd29292b27a4cb9b52721de
Inf1delis <v.mamedov@g.nsu.ru>
2019-09-06 23:51:41 +0700

    smiles

--- writing_style.py
+++ writing_style.py
@@ -2,6 +2,7 @@ from os import system as os
 import sys
 import xerox
 import pynput
+import random
 from gc import collect
 from platform import system
 
@@ -15,32 +16,28 @@ WINDOWS_MODE = system() == 'Windows'
 
 # THERE ARE A LOT OF SHIT IN THE CODE! BE CAREFUL!
 shift = Key.shift
-shift_str = str(shift)
 option = Key.ctrl if WINDOWS_MODE else Key.alt
 left = Key.left
 enter = Key.enter
 ctrl = Key.ctrl if WINDOWS_MODE else Key.cmd_l
 space = Key.space
-space_str = str(space)
 delete = Key.delete
-backspace = Key.backspace
-backspace_str = str(backspace)
 symbols = ',.:;<=>?@[]^-_`{"|"}§±'
 symbols += ''.join([chr(i) for i in range(20, 41)])
 vk_smiles = ['&#128127;', '&#128163;', '&#128552;', '&#128158;', '&#128539;', '&#128528;', '&#128580;', '&#128545;',
              '&#128169;', '&#128526;', '&#128517;', '&#128527;', '&#128520;', '&#128514;', '&#128133;']
 # здесь можно конечно пробежаться в цикле for in range, но не все смайлы крутые
+clear = lambda: os('cls' if WINDOWS_MODE else 'clear')
 # WARNING !!!!!!!
 
 word_buf = []
-prg_exit = False
+exit = False
 mode = False
 keybrd = pynput.keyboard.Controller()
-letter_counter = 0
 
 
-def random_smile():
-    index = random.randint(0, len(vk_smiles) - 1)
+def random_smile(smiles):
+    index = random.randint(0, len(smiles) - 1)
     return vk_smiles[index]
 
 
@@ -54,107 +51,47 @@ def dawn_mode(key):
 
 
 def algorithm():
-    global word_buf, letter_counter
-    # word_string = ''.join([str(ch) for ch in word_buf])
-    # # копируем наше слово в буфер обмена
-    # xerox.copy(word_string)
-    #
-    # # выделяем введенное слово
-    # with keybrd.pressed(shift, option):
-    #     keybrd.press(left)
-    #     keybrd.release(left)
-    #
-    #
-    # with keybrd.pressed(ctrl):
-    #     keybrd.press(LANG)
-    #     keybrd.release(LANG)
-    #
-    #     if WINDOWS_MODE:  # особенность
-    #         keybrd.press(delete)
-    #         keybrd.release(delete)
+    global word_buf
+    word_string = ''.join([str(ch) for ch in word_buf])
+    # копируем наше слово в буфер обмена
+    xerox.copy(word_string)
 
+    # выделяем введенное слово
+    with keybrd.pressed(shift, option):
+        keybrd.press(left)
+        keybrd.release(left)
 
-    # ХУЯРИМ ЧЕРЕЗ БЭКСПЕЙСЫ
+    with keybrd.pressed(ctrl):
+        keybrd.press(LANG)
+        keybrd.release(LANG)
 
-    for i in range(letter_counter+1):
-        keybrd.press(backspace)
-        keybrd.release(backspace)
-
-    buff_length = len(word_buf)
-    i = 0
-    while i < buff_length:
-        key_str = word_buf[i]
-        if len(key_str) == 1:
-            print(key_str)
-            keybrd.press(key_str)
-            keybrd.release(key_str)
-
-        elif key_str == shift_str:
-            try:
-                while word_buf[i+1] == shift_str:
-                    i += 1
-                with keybrd.pressed(shift):
-                    i += 1
-                    key_str = word_buf[i]
-
-                    print(key_str)
-                    keybrd.press(key_str)
-                    keybrd.release(key_str)
-            except:
-                print('idi nahui')
-        i += 1
-
+        if WINDOWS_MODE:  # особенность
+            keybrd.press(delete)
+            keybrd.release(delete)
     # ставим пробел после записи
     keybrd.press(space)
     keybrd.release(space)
 
-    # del word_string
-    letter_counter = 0
+    del word_string
     del word_buf[:]
 
 
 def on_press(key):
-    global word_buf, prg_exit, letter_counter, mode
+    global word_buf, exit
     key = str(key)
 
-    # if key[1] in symbols:
-    #     pass
-    #
-    # elif len(key) == 3:       # отсеивает командные кнопки
-    #     word_buf.append(dawn_mode(key))
-    #
-    # elif len(word_buf) != 0:
-    #     if key == 'Key.space':
-    #         word_buf.append(random_smile(vk_smiles))
-    #     elif key == 'Key.backspace':
-    #         word_buf.pop()
-
-    if key == 'Key.esc':
-        prg_exit = True
-
-    elif len(key) == 3:       # отсеивает командные кнопки
-        letter_counter += 1
-        key_str = key[1]
-        # word_buf.append(dawn_mode(key))
-        mode = not mode
-        if key_str != '?' and mode:
-            word_buf.append(shift_str)
-
-        # if key_str == '?':
-        #     word_buf.append(shift_str)
-        #     key_str = '7'
-
-        word_buf.append(key_str.lower())
-
+    if len(key) == 3:       # отсеивает командные кнопки
+        word_buf.append(dawn_mode(key))
+    elif key == 'Key.space':
+        word_buf.append(random_smile(vk_smiles))
     elif len(word_buf) != 0 and key == 'Key.backspace':
         word_buf.pop()
-    elif key == shift_str:
-        word_buf.append(key)
-
+    elif key == 'Key.esc':
+        exit = True
 
 
 def on_release(key):
-    if prg_exit or letter_counter != 0 and \
+    if exit or len(word_buf) != 0 and \
              (key == space or key == enter):
         return False
 
@@ -164,12 +101,10 @@ while True:
             on_press=on_press,
             on_release=on_release) as listener:
         listener.join()     # мониторинг клавиатуры
+        del listener
 
-    print(word_buf)
-
-    if prg_exit == True:
-        del keybrd
-        break
-
-    if letter_counter != 0:
-        algorithm()
+    if exit == True:
+        sys.exit()
+    algorithm()
+    collect()       # зовет сборщик мусора
+    clear()         # очищает консоль
